fe77d0dbd3332d62b623c40812cd8fa2c7273a47
Constantius Neil CHANDRA <cchandra@i-tracing.com>
2025-06-02 12:08:20 +0800

    add /Users route

---
+++ netlify/functions/users.js
@@ -0,0 +1,38 @@
+// placeholder comments
+
+const axios = require('axios');
+
+exports.handler = async function(event, context) {
+  const { path, httpMethod } = event;
+
+  // Define the routes and corresponding environment variables
+  const apiUrl = process.env.API_URL_USERS
+  const apiKey = process.env.API_KEY_USERS
+
+  // Forward the request to the external API
+  try {
+    const headers = {
+      'x-api-client-token': apiKey,
+      ...(event.headers || {}),
+    };
+
+    const params = {
+      method: httpMethod,
+      url: apiUrl,
+      headers: headers,
+      data: event.body ? JSON.parse(event.body) : null,
+    };
+
+    const response = await axios(params);
+
+    return {
+      statusCode: response.status,
+      body: JSON.stringify(response.data),
+    };
+  } catch (error) {
+    return {
+      statusCode: error.response ? error.response.status : 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
